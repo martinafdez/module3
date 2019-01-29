0a2ac5a681ec9cb6e54983ce7bde1fc51365ca72
Martina Fernandez <martinalucia.fdez@gmail.com>
2019-01-29 10:35:51 +0000

    minor change

--- ch02_Validation/ch02_martina.py
+++ ch02_Validation/ch02_martina.py
@@ -9,50 +9,50 @@ Created on Wed Jan 23 13:53:55 2019
 ##########Chapter 2: Validation###########
 
 
-#####Book tasks: Revising user inputs ####
-##print("What’s your age?")
-##Age = input()
-##
-##print ("What’s your age?")
-##age = int(input())
-##type(age)
-##
-##Option = input("Please input yes or no ").lower()
+####Book tasks: Revising user inputs ####
+#print("What’s your age?")
+#Age = input()
 #
-####Task 1#####
-#print("\n *--------Task 1: An example for validating string length--------* \n")
+#print ("What’s your age?")
+#age = int(input())
+#type(age)
 #
-#password = (input("Please enter your password: "))
-#print(password)
+#Option = input("Please input yes or no ").lower()
 #
-#if len(password) > 4:
-#    print("Password too long. Try again")
-#elif len(password) < 4:
-#    print("Password too short. Try again")
-#else:
-#    print("Success. Continue!")
+###Task 1#####
+print("\n *--------Task 1: An example for validating string length--------* \n")
+
+password = (input("Please enter your password: "))
+print(password)
+
+if len(password) > 4:
+    print("Password too long. Try again")
+elif len(password) < 4:
+    print("Password too short. Try again")
+else:
+    print("Success. Continue!")
 #    
 #    
 #    
-####Task 2#####  
-#
-#print("\n *--------Task 2: Using while true infinite loop--------* \n")
-#
-#print('***choice****')
-#print('1. Display my name')
-#print('2. Display my age')
-#print('3. Display my city')
-#
-#choice=0
-#while choice < 1 or choice > 3:   
-#    choice = int(input('What is your choice? '))
-#
-#if choice == 1:   
-#    print('Ms Wu')
-#elif choice == 2:   
-#    print('5 years old')
-#elif choice == 3:   
-#    print('London')
+###Task 2#####  
+
+print("\n *--------Task 2: Using while true infinite loop--------* \n")
+
+print('***choice****')
+print('1. Display my name')
+print('2. Display my age')
+print('3. Display my city')
+
+choice=0
+while choice < 1 or choice > 3:   
+    choice = int(input('What is your choice? '))
+
+if choice == 1:   
+    print('Ms Wu')
+elif choice == 2:   
+    print('5 years old')
+elif choice == 3:   
+    print('London')
 #    
 
 ####Book task: Handling errorful input####
@@ -79,14 +79,14 @@ elif choice == 3:
 
 
 
-####Book task: Further on validation######
-#class Spam(object):   
-#    def __init__(self, description, value):      
-#        if not description or value <= 0:         
-#            raise ValueError      
-#        self.description = description      
-#        self.value = value
-#        
-#number=int(input("enter spam description: "))
-#s = Spam('s', number)
-#print(s.value)
+###Book task: Further on validation######
+class Spam(object):   
+    def __init__(self, description, value):      
+        if not description or value <= 0:         
+            raise ValueError      
+        self.description = description      
+        self.value = value
+        
+number=int(input("enter spam description: "))
+s = Spam('s', number)
+print(s.value)
